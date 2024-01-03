03193ee7d0bf608e3a8231c7769f407b416ea306
sharadv07 <sharad.vidyapathy@gmail.com>
2024-01-03 15:04:03 -0500

    updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,14 @@ output "resourcegroupid" {
   value = azurerm_resource_group.rg-poc.id
 
 }
+output "resource_group_name" {
+  description = "Resource Group Name"
+  value       = azurerm_resource_group.appgrp.name
+  sensitive   = false
+}
+
+output "resource_group_location" {
+  description = "Resource Group Location"
+  value       = azurerm_resource_group.appgrp.location
+  sensitive   = false
+}
